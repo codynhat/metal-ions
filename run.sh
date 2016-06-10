e3a6c9160c1b618d84164343bca673d98d7d9e73
Cody Hatfield <cody@hatfieldworks.com>
2016-06-10 12:31:00 -0700

    Updated run.sh

--- run.sh
+++ run.sh
@@ -6,109 +6,80 @@ INPUT_FILE='/home7/ereister/Bioinformatics_Spring2016/raw_data/test.txt'
 CLUSTER_SIZE=23
 CM='/home7/ereister/Bioinformatics_Spring2016/motifs/cmdatabase.cm'
 
+module load python
+module load blast
+
 python blast_and_clustering/blast_pipeline.py $BLAST_TEMP_PATH $OUTPUT_PATH $CLUSTER_SIZE < $INPUT_FILE
 
 module load CMfinder
 module load infernal/1.1.1
 
-##Run first round of CMfinder
+##Run first round of CMfinder, searching for single stem loops
+
 cd $OUTPUT_PATH
-for file in **/**/*
+for file in **/*.fasta
 do
     cmfinder.pl -b -s 1 -n 5 $file
-done
-##Make a directory "motif" in each accession number's folder
-
-shopt -s globstar
-for dir in $OUTPUT_PATH/*/
-do
-mkdir -- "$dir/motif"
-done
-
-##move all "motif" and "cm" files to motif folder so next CMfinder doesn't run on them as well as the original fasta
-
-for i in $OUTPUT_PATH/**/*motif*
-do
-    mv $i */motif
-done
-
-for x in $OUTPUT_PATH/**/*cm*
-do
-    mv $x */motif
-done
-
-##Run second round of cmfinder
-
-for file in $OUTPUT_PATH/**/*
-do
+    wait
     cmfinder.pl -b -s 2 -n 5 $file
-done
-
-#move items from motif folder back into main folder for next step
-for file in $OUTPUT_PATH/**/motif/*
-do
-    mv $file
-done
-
-##Run Combmotif on each file set
-for file in $OUTPUT_PATH/**/*
-do
+    wait
     CombMotif.pl $file $file.motif*
 done
 
 ##For each cluster set, combine their stockholm files into one
+cd $OUTPUT_PATH
+for file in **/*cluster_0*motif*
+do
+    cat $file >> $file.combined_motifs.sto
+done
 
-for file in $OUTPUT_PATH/**/*cluster_0*motif*
+for file in **/*cluster_1*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_1*motif*
+for file in **/*cluster_2*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_2*motif*
+for file in **/*cluster_3*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_3*motif*
+for file in **/*cluster_4*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_4*motif*
+for file in **/*cluster_5*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_5*motif*
+for file in **/*cluster_6*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_6*motif*
+for file in **/*cluster_7*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_7*motif*
+for file in **/*cluster_8*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
-for file in $OUTPUT_PATH/**/*cluster_8*motif*
-do
-    cat $file >> $file.combined_motifs.sto
-done
-for file in $OUTPUT_PATH/**/*cluster_9*motif*
+for file in **/*cluster_9*motif*
 do
     cat $file >> $file.combined_motifs.sto
 done
 
 #Run stockholm files into cmscan
-
-for file in $OUTPUT_PATH/**/**.sto
+cd $OUTPUT_PATH
+for file in **/**.sto
 do
     cmscan --tblout $file.cmsc_out.txt $CM $file
 done
 
 ##Remove all leftover  cm files
-
-for file in $OUTPUT_PATH/**/*cm
+cd $OUTPUT_PATH
+for file in **/*cm*
 do
     rm $file
 done
